44bfaeac21f5e151d2ff2e6bec5990c00a24d31a
eli490 <masosoelvin@gmail.com>
2022-09-08 03:20:36 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
+
 #include <unistd.h>
 
 /**
- * main - prints exactly "and that piece of art is useful"
- * - Dora korpar, 2015-10-19",
- *   followed by a new line, to the standard error.
- *   Return: Always 0 (Success)
- */
+ *
+ *  * main - Entry point
+ *
+ *   * Return:1
+ *
+ *    */
+
 int main(void)
+
 {
-	printf(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
+
+	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, ch, sizeof(ch));
+
 	return (1);
+
 }
