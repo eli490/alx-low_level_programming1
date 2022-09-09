7ef0f5915bfbfa47db314cbbac577062d16c97f7
eli490 <masosoelvin@gmail.com>
2022-09-09 04:19:29 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * * main - Entry point
+ * *
+ * * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha;
+	for (alpha = 'a'; alpha <= '2'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}
