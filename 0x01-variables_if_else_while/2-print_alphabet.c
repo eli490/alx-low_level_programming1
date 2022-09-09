9f19e24a881c520b02193e353e3d73d3860c4d71
eli490 <masosoelvin@gmail.com>
2022-09-09 04:28:59 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,12 +5,16 @@
  * * Return: Always 0 (Success)
  */
 int main(void)
+
 {
 	char alpha;
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 	putchar(alpha);
 	}
+
 	putchar('\n');
+
 	return (0);
+
 }
