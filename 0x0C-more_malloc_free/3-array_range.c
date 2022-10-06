1566104c3ec630b1e969bf0ed6b5b2231503ffe5
eli490 <masosoelvin@gmail.com>
2022-10-06 10:40:46 -0700

    concatenates two strings

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * array_range - creates an array of integers.
+ * @min: smalles int.
+ * @max: largest int.
+ * Return:pointer to the array or NULL.
+ */
+
+int *array_range(int min, int max)
+{
+	int *p = NULL, *p_tmp = NULL;
+
+	if (min > max)
+		return (NULL);
+
+	p = p_tmp = malloc(sizeof(int) * (max - min + 1));
+	if (!p)
+		return (NULL);
+	while (min <= max)
+	{
+		*p_tmp++ = min++;
+	}
+	return (p);
+}
